031a1401f1ea24bc39bfbde0ca5dce5212e7e30d
MariaKobryn <kobinmary2508@gmail.com>
2021-09-29 16:57:45 +0300

    first master commit

---
+++ Lab2.1/Лаб 2.1.cpp
@@ -0,0 +1,31 @@
+// Lab2.1.cpp
+// < Кобрин Марії >
+// Лабораторна робота № 2.1
+// Лінійні програми.
+// Варіант 10
+
+#include <iostream>
+#include <cmath>
+
+using namespace std;
+
+int main()
+{
+	double Pi = 4 * atan(1.); // число пі
+
+	double a; // вхідний параметр
+	double z1; // результат обчислення 1-го виразу
+	double z2; // результат обчислення 2-го виразу
+
+	cout << "a = "; cin >> a;
+
+	z1 = (sin((Pi/2.0)+3*a))/(1-sin(3*a-Pi));
+	z2 = 1/tan(5.0/4*Pi+3.0/2*a);
+
+	cout << endl;
+	cout << "z1 = " << z1 << endl;
+	cout << "z2 = " << z2 << endl;
+
+	cin.get();
+	return 0;
+}
